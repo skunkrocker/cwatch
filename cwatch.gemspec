39823ce8dba843d314138b63f3da6356bc2eab8e
Nikola Spaiskov <nikola.spasikov@soprasteria.com>
2021-08-20 19:45:11 +0200

    add colors to the table

--- cwatch.gemspec
+++ cwatch.gemspec
@@ -22,4 +22,5 @@ spec = Gem::Specification.new do |s|
   s.add_runtime_dependency('gli', '~> 2.20.1')
   s.add_runtime_dependency('httparty', '~> 0.18.1')
   s.add_runtime_dependency('terminal-table', '~> 3.0.1')
+  s.add_runtime_dependency('colorize', '~> 0.8.1')
 end
--- lib/util/table.rb
+++ lib/util/table.rb
@@ -1,21 +1,25 @@
+require 'colorize'
 require 'terminal-table'
 
 module Table
 
   def print(buy, sell, spot)
     rows = []
-    heads = ['Buy €', 'Sell €', 'Spot €']
-
+    heads = ['Buy €'.light_blue, 'Sell €'.light_red, 'Spot €'.light_blue]
     buy_price = buy['data']['amount']
     sell_price = sell['data']['amount']
     spot_price = spot['data']['amount']
 
-    rows << [buy_price, sell_price, spot_price]
-    table = Terminal::Table.new :rows => rows, :headings => heads, :style => { :border_x => "-", :border_i => "°" }
+    rows << [buy_price.light_blue, sell_price.light_red, spot_price.light_blue]
+    table = Terminal::Table.new :rows => rows, :headings => heads, :style => { :border_top => false,
+                                                                               :border_left => false,
+                                                                               :border_right => false,
+                                                                               :border_bottom => false }
+    table.add_separator
+    table.add_row ['Refresh'.yellow, { :value => '30s'.yellow, :colspan => 2, :alignment => :left }]
 
     system("clear")
     puts table
-    #puts 'each 30s'
   end
 
   module_function :print
