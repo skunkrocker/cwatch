a27a42513f907be260f5eb83028d163bd3a15745
Nikola Spaiskov <nikola.spasikov@soprasteria.com>
2021-08-23 19:21:40 +0200

    print table with roi list

--- lib/util/table.rb
+++ lib/util/table.rb
@@ -25,6 +25,7 @@ module Table
   def render_coins(coin_entries)
     rows = []
     headings = ['Coin Amount'.green, 'Buy Rate'.green, 'CB Fees'.green, 'Token'.green, 'Fiat'.green]
+
     coin_entries.sort_by { |c| c[3] }.each_with_index do |coin, index|
       if index % 2 == 0
         rows << coin.map { |c| c.to_s.light_red }
@@ -32,14 +33,40 @@ module Table
         rows << coin.map { |c| c.to_s.light_blue }
       end
     end
+
+    table = Terminal::Table.new :rows => rows, :headings => headings, :style => { :border_top => false,
+                                                                                  :border_left => false,
+                                                                                  :border_right => false,
+                                                                                  :border_bottom => false }
+    system("clear")
+    puts table
+  end
+
+  def render_roi(roi_results)
+    rows = []
+    total_roi = 0
+    headings = ['Amount'.green, 'Buy Price'.green, 'Sell Price'.green, 'ROI €'.green, 'ROI %'.green, 'Token'.green, 'Fiat'.green]
+
+    roi_results.each_with_index do |roi, index|
+      total_roi += roi[3]
+      if index % 2 == 0
+        rows << roi.map { |c| c.to_s.light_red }
+      else
+        rows << roi.map { |c| c.to_s.light_blue }
+      end
+    end
+
     table = Terminal::Table.new :rows => rows, :headings => headings, :style => { :border_top => false,
                                                                                   :border_left => false,
                                                                                   :border_right => false,
                                                                                   :border_bottom => false }
 
+    table.add_separator
+    table.add_row ['Total ROI € '.yellow, { :value => total_roi.to_s.yellow, :colspan => 2, :alignment => :left }]
+
     system("clear")
     puts table
   end
 
-  module_function :print, :render_coins
+  module_function :print, :render_coins, :render_roi
 end
