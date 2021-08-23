d3536d2148f6c9100d3a75c650d326a02128cbc8
Nikola Spaiskov <nikola.spasikov@soprasteria.com>
2021-08-23 17:08:36 +0200

    extract db logic to a class

---
+++ lib/util/database.rb
@@ -0,0 +1,45 @@
+require 'fileutils'
+
+class Database
+
+  def initialize
+    db_file = "#{Dir.home}/.portfolio.db"
+    FileUtils.touch db_file unless File.exist? db_file
+
+    begin
+      @db = SQLite3::Database.new db_file
+      @db.execute <<-SQL
+          CREATE TABLE IF NOT EXISTS coin(
+            amount REAL,
+            rate REAL,
+            fees REAL, 
+            token TEXT,
+            fiat TEXT
+           )
+      SQL
+    rescue SQLite3::Exception => e
+      puts 'failed to load the db'.red
+    end
+  end
+
+  def add(options)
+    begin
+      @db.execute "INSERT INTO coin (amount, rate, fees, token, fiat) VALUES (?,?,?,?,?)",
+                  options[:a], options[:r], options[:c], options[:t], options[:f]
+    rescue SQLite3::Exception => e
+      puts e.message || 'could not add the coin buy to the portfolio'
+    end
+  end
+
+  def all
+    begin
+      @db.execute "select * from coin"
+    rescue SQLite3::Exception => e
+      puts e.message || 'could not read the coins from portfolio'
+    end
+  end
+
+  def dispose
+    @db.close if @db
+  end
+end
